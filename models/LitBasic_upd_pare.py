b6998fc17a8dc98485b60cfee33845d173582a92
IlyaBoss2414 <121303926+IlyaBoss2414@users.noreply.github.com>
2025-02-12 19:49:59 +0300

    Update LitBasic_upd_pare.py

--- models/LitBasic_upd_pare.py
+++ models/LitBasic_upd_pare.py
@@ -3,6 +3,7 @@ import torch.nn.functional
 import torchmetrics
 from torch import optim
 from lion_pytorch import Lion
+import torch.nn as nn
 
 class LitBasic_upd_pare(L.LightningModule):
     def __init__(self, model, only_head=False):
